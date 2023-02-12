ef8d471363a7a74d4abe1d0ab955b0425ac8acc9
Jacob Dawson <jacob.dawson@me.com>
2023-02-12 12:25:37 -0500

    abstract loading function

---
+++ constants.py
@@ -0,0 +1,52 @@
+# Author: Jacob Dawson
+#
+# This file just collects a few constants and functions we use repeatedly in
+# other scripts
+
+import os
+import pandas as pd
+
+seed = 3
+
+def loadData():
+    print('Loading data')
+
+    list_of_files = {}
+    for (dirpath, dirnames, filenames) in os.walk('hourly_weather'):
+        for filename in filenames:
+            if filename.endswith('.csv'):
+                list_of_files[filename] = os.sep.join([dirpath, filename])
+
+    df = pd.read_csv('hourly_weather/temperature.csv')
+
+    y = df['New York'] # let's try to predict the temps in New York!
+    y = pd.to_numeric(y)
+    y = y.fillna(value=-1)
+    #print(y)
+
+    i=0
+    for k,v in list_of_files.items():
+        if k=='temperature.csv':
+            continue
+        if k=='city_attributes.csv':
+            continue
+
+        # I want to encode this but it's become a headache. We'll circle back.
+        if k=='weather_description.csv':
+            continue
+        '''# we need to handle strings and floats differently
+        if k=='weather_description.csv':
+            i+=1
+            string_df = pd.read_csv(v)
+            enc = OrdinalEncoder().set_params(encoded_missing_value=-1)
+            df_without_datetime = string_df.drop(['datetime'], axis=1)
+            df_without_datetime = enc.fit_transform(df_without_datetime)
+            for column in df_without_datetime:
+                string_df[column] = df_without_datetime[column]
+            df = df.merge(string_df, on='datetime', suffixes=(None,str(i)))
+        else:
+            i+=1
+            df = df.merge(pd.read_csv(v), on='datetime', suffixes=(None,str(i)))'''
+
+        i+=1
+        df = df.merge(pd.read_csv(v), on='datetime', suffixes=(None,str(i)))
--- sklearnPredictor.py
+++ sklearnPredictor.py
@@ -9,56 +9,15 @@
 # in New York on a certain day?
 
 import pandas as pd
-import numpy as np
-from IPython.display import display
-import os
+#import numpy as np
+from constants import *
+#from IPython.display import display
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
-seed = 3
 
-print('Loading data')
-
-list_of_files = {}
-for (dirpath, dirnames, filenames) in os.walk('hourly_weather'):
-    for filename in filenames:
-        if filename.endswith('.csv'):
-            list_of_files[filename] = os.sep.join([dirpath, filename])
-
-df = pd.read_csv('hourly_weather/temperature.csv')
-
-y = df['New York'] # let's try to predict the temps in New York!
-y = pd.to_numeric(y)
-y = y.fillna(value=-1)
-#print(y)
-
-i=0
-for k,v in list_of_files.items():
-    if k=='temperature.csv':
-        continue
-    if k=='city_attributes.csv':
-        continue
-
-    # I want to encode this but it's become a headache. We'll circle back.
-    if k=='weather_description.csv':
-        continue
-    '''# we need to handle strings and floats differently
-    if k=='weather_description.csv':
-        i+=1
-        string_df = pd.read_csv(v)
-        enc = OrdinalEncoder().set_params(encoded_missing_value=-1)
-        df_without_datetime = string_df.drop(['datetime'], axis=1)
-        df_without_datetime = enc.fit_transform(df_without_datetime)
-        for column in df_without_datetime:
-            string_df[column] = df_without_datetime[column]
-        df = df.merge(string_df, on='datetime', suffixes=(None,str(i)))
-    else:
-        i+=1
-        df = df.merge(pd.read_csv(v), on='datetime', suffixes=(None,str(i)))'''
-
-    i+=1
-    df = df.merge(pd.read_csv(v), on='datetime', suffixes=(None,str(i)))
+df, y = loadData()
 
 print('Preprocessing data')
 
@@ -67,8 +26,6 @@ df = df.drop(['datetime'], axis=1)
 # and encode all as numeric:
 df = df.apply(pd.to_numeric, errors='raise', downcast='float')
 df = df.fillna(-1)
-
-#print(df.shape)
 
 df = df.shift(periods=-24, fill_value=-1)
 
