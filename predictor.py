55b68bed91aaa1c16ff980fa89aa46e467d04ec9
Jacob Dawson <jacob.dawson@me.com>
2023-02-12 12:07:52 -0500

    remove comments

--- predictor.py
+++ predictor.py
@@ -16,7 +16,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
-#from sklearn.preprocessing import OrdinalEncoder
 seed = 3
 
 print('Loading data')
@@ -28,7 +27,6 @@ for (dirpath, dirnames, filenames) in os.walk('hourly_weather'):
             list_of_files[filename] = os.sep.join([dirpath, filename])
 
 df = pd.read_csv('hourly_weather/temperature.csv')
-#display(df)
 
 y = df['New York'] # let's try to predict the temps in New York!
 y = pd.to_numeric(y)
@@ -58,41 +56,21 @@ for k,v in list_of_files.items():
     else:
         i+=1
         df = df.merge(pd.read_csv(v), on='datetime', suffixes=(None,str(i)))'''
+
     i+=1
-    
     df = df.merge(pd.read_csv(v), on='datetime', suffixes=(None,str(i)))
 
 print('Preprocessing data')
-
-# the idea here is that we want an algorithm which can predict the temperature
-# of new york at some hour in the future, given previous recorded values from
-# all over the world. As such, we'll leave in the recorded values for new york,
-# even though this seems a bit strange--a real weather station would have that
-# data (obs) if they were making a prediction
 
 # we don't want to let it cheat by looking at the date:
 df = df.drop(['datetime'], axis=1)
 # and encode all as numeric:
 df = df.apply(pd.to_numeric, errors='raise', downcast='float')
-#display(df)
 df = df.fillna(-1)
-#display(df)
-
-'''
-# let's do our splitting first, before we mess up our test set!
-X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.25, random_state=seed, shuffle=False)
-'''
 
 #print(df.shape)
 
 df = df.shift(periods=-24, fill_value=-1)
-'''
-df = df.iloc[0:(df.shape[0]//(7*24))*24*7]
-dfs = np.array(np.array_split(df, (df.shape[0]//24)//7)) # break into weeks
-dfs = np.reshape(dfs, (-1, dfs.shape[1] * dfs.shape[2])) # painful! I hate to do it!
-y = y[0:(y.size//(7*24))*24*7]
-y = np.reshape(np.array(y), (-1,7*24))
-'''
 
 X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.25, random_state=seed, shuffle=True)
 regr = RandomForestRegressor(max_depth=10, random_state=seed, n_estimators=25)
